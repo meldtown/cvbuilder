3e7f2904ceed90122e64f876e40bb676487dd94c
VitaliyM <vitaliym@rabota.ua>
2016-05-17 10:22:09 +0300

    Tests for added items in personal info

--- spec/PersonalInfoModelSpec.js
+++ spec/PersonalInfoModelSpec.js
@@ -18,5 +18,26 @@ describe('PersonalInfoModel', function () {
 		expect(model.personalInfo.name.isValid()).toBeFalsy();
 		expect(model.personalInfo.name.error()).toBe('This field is required.');
 	});
+	it('should require last name', function () {
+		model.personalInfo.beginEdit();
+		expect(model.personalInfo.surName.isValid()).toBeTruthy();
+		model.personalInfo.surName('');
+		expect(model.personalInfo.surName.isValid()).toBeFalsy();
+		expect(model.personalInfo.surName.error()).toBe('This field is required.');
+	});
+	it('should require the date birth', function () {
+		model.personalInfo.beginEdit();
+		expect(model.personalInfo.dateBirth.isValid()).toBeTruthy();
+		model.personalInfo.dateBirth('');
+		expect(model.personalInfo.dateBirth.isValid()).toBeFalsy();
+		expect(model.personalInfo.dateBirth.error()).toBe('This field is required.');
+	});
+	it('should require sex', function () {
+		model.personalInfo.beginEdit();
+		expect(model.personalInfo.sex.isValid()).toBeTruthy();
+		model.personalInfo.sex('');
+		expect(model.personalInfo.sex.isValid()).toBeFalsy();
+		expect(model.personalInfo.sex.error()).toBe('This field is required.');
+	});
 });
 
