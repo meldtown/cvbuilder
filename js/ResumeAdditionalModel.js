4ba61945cb8ba0b963dcac655e1f4fdb27eacff4
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-05-24 13:40:10 +0300

    backend

--- js/ResumeAdditionalModel.js
+++ js/ResumeAdditionalModel.js
@@ -15,9 +15,27 @@ function ResumeAdditionalModel (parent, data) {
 		return mapper.toJS(model);
 	};
 
-	model.save = function () {};
+	model.save = function () {
+		if (model.errors().length === 0) {
+			backend.post(parent.api + '/resume/' + parent.resumeId + '/additional', model.toJS())
+				.success(function (id) {
+					model.id(id);
+					model.commit();
+				})
+				.fail(function (jqXHR) {
+					if (jqXHR.status === 400) {
+						model.handleBarRequestResponse(jqXHR);
+					}
+				});
+		}
+	};
 
-	model.remove = function () {};
+	model.remove = function () {
+		backend.remove(parent.api + '/resume/' + parent.resumeId + '/additional/' + model.id())
+			.success(function () {
+				parent.additional.remove(model);
+			});
+	};
 
 	InitEditableModel(model, 'additional');
 	InitBadRequestResponseHandler(model);
