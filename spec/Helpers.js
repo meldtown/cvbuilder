77162b2b1099bb3d7c2d56853607eade5365fc87
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-06-15 15:52:50 +0300

    extract dictionary for unit tests

---
+++ spec/Helpers.js
@@ -0,0 +1,153 @@
+function getDummyDictionary () {
+	return {
+		activityLevel: [
+			{
+				id: 1,
+				ru: 'activity level 1',
+				en: 'activity level 1',
+				ua: 'activity level 1'
+			},
+			{
+				id: 2,
+				ru: 'activity level 2',
+				en: 'activity level 2',
+				ua: 'activity level 2'
+			}
+		],
+		schedule: [
+			{
+				id: 1,
+				ru: 'schedule 1',
+				en: 'schedule 1',
+				ua: 'schedule 1'
+			},
+			{
+				id: 2,
+				ru: 'schedule 2',
+				en: 'schedule 2',
+				ua: 'schedule 2'
+			}
+		],
+		branch: [
+			{
+				id: 1,
+				ru: 'branch 1',
+				en: 'branch 1',
+				ua: 'branch 1'
+			},
+			{
+				id: 2,
+				ru: 'branch 2',
+				en: 'branch 2',
+				ua: 'branch 2'
+			}
+		],
+		experience: [
+			{
+				id: 1,
+				ru: 'experience 1',
+				en: 'experience 1',
+				ua: 'experience 1'
+			},
+			{
+				id: 2,
+				ru: 'experience 2',
+				en: 'experience 2',
+				ua: 'experience 2'
+			}
+		],
+		city: [
+			{
+				id: 1,
+				ru: 'city 1',
+				en: 'city 1',
+				ua: 'city 1'
+			},
+			{
+				id: 2,
+				ru: 'city 2',
+				en: 'city 2',
+				ua: 'city 2'
+			}
+		],
+		educationType: [
+			{
+				id: 1,
+				ru: 'education 1',
+				en: 'education 1',
+				ua: 'education 1'
+			},
+			{
+				id: 2,
+				ru: 'education 2',
+				en: 'education 2',
+				ua: 'education 2'
+			}
+		],
+		currency: [
+			{
+				id: 1,
+				ru: 'uah',
+				en: 'uah',
+				ua: 'uah'
+			},
+			{
+				id: 2,
+				ru: 'usd',
+				en: 'usd',
+				ua: 'usd'
+			}
+		],
+		sex: [
+			{
+				id: 0,
+				ru: 'female',
+				ua: 'female',
+				en: 'female'
+			},
+			{
+				id: 1,
+				ru: 'male',
+				ua: 'male',
+				en: 'male'
+			}
+		],
+		resource: {
+			requiredMessage: {
+				ru: 'requiredMessage',
+				en: 'requiredMessage',
+				ua: 'requiredMessage'
+			},
+			wrongFormat: {
+				ru: 'wrongFormat',
+				en: 'wrongFormat',
+				ua: 'wrongFormat'
+			},
+			educationUniversityNameLabel: {
+				ru: 'educationUniversityNameLabel',
+				en: 'educationUniversityNameLabel',
+				ua: 'educationUniversityNameLabel'
+			},
+			addPhoto: {
+				ru: 'addPhoto',
+				en: 'addPhoto',
+				ua: 'addPhoto'
+			},
+			percentMore: {
+				ru: 'percentMore',
+				en: 'percentMore',
+				ua: 'percentMore'
+			},
+			addExperience: {
+				ru: 'addExperience',
+				en: 'addExperience',
+				ua: 'addExperience'
+			},
+			addElseExperience: {
+				ru: 'addElseExperience',
+				en: 'addElseExperience',
+				ua: 'addElseExperience'
+			}
+		}
+	};
+}
